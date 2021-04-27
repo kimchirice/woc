a45c7d77c37e32cbc73c96f556d2e54f29cdeedd
debubz <bao.es.hoang2@gmail.com>
2021-04-27 11:57:53 +1000

    changed navbar to function

--- web/src/components/navbar/NavBar.jsx
+++ web/src/components/navbar/NavBar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { AppBar, Toolbar, Button, Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/";
 import { Link } from "react-router-dom";
 import logo from "../../res/img/2.png";
 import "./NavBar.css";
@@ -8,33 +9,38 @@ import "./NavBar.css";
     Handing main navigation bar for site
 
     - NavItems might be change due to authentication status
+    - Move regular css to MUI css
 */
 
-class NavBar extends React.Component {
-    // renderNavItems = () => NavItems.map((item, index) => <NavItem index={index} item={item} />);
+const styles = makeStyles((theme) => ({
+    flex: {
+        flexGrow: 1,
+    },
+}));
 
-    render() {
-        return (
-            <>
-                <AppBar style={{ flexGrow: 1 }}>
-                    <Toolbar>
-                        <Link to="/">
-                            <div style={{ flexGrow: 1 }}>
-                                <img className="logo-image" src={logo}></img>
-                            </div>
-                        </Link>
-                        <Typography variant="h6" style={{ flexGrow: 1 }}></Typography>
-                        <Link to="/login">
-                            <Button>Login</Button>
-                        </Link>
-                        <Link to="/signup">
-                            <Button>Sign up</Button>
-                        </Link>
-                    </Toolbar>
-                </AppBar>
-            </>
-        );
-    }
-}
+const NavBar = () => {
+    const classes = styles();
+
+    return (
+        <>
+            <AppBar style={{ flexGrow: 1 }}>
+                <Toolbar>
+                    <Link to="/">
+                        <div className={classes.flex}>
+                            <img className="logo-image" src={logo}></img>
+                        </div>
+                    </Link>
+                    <Typography variant="h6" className={classes.flex}></Typography>
+                    <Link to="/login">
+                        <Button>Login</Button>
+                    </Link>
+                    <Link to="/signup">
+                        <Button>Sign up</Button>
+                    </Link>
+                </Toolbar>
+            </AppBar>
+        </>
+    );
+};
 
 export default NavBar;
