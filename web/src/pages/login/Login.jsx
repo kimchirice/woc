cc939c5bd24df3ee12bdc059c4ce7a3a83cd7b2f
Max Du <kimchi@u12.co>
2021-05-05 09:57:42 +1000

    [Ref] move Copyright from both Login page and SignUp up, into a seperate component. more DRY

--- web/src/pages/login/Login.jsx
+++ web/src/pages/login/Login.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
@@ -12,24 +12,7 @@ import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-import { useState } from "react";
-
-/*
-    Login Page
-*/
-
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://womenofcolour.org.au/">
-        Women of Colour
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  ); 
-}
+import Copyright from '../../components/copyright/Copyright'
 
 const useStyles = makeStyles((theme) => ({
   paper: {
--- web/src/pages/signUp/SignUp.jsx
+++ web/src/pages/signUp/SignUp.jsx
@@ -11,19 +11,7 @@ import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
-
-function Copyright() {
-    return (
-        <Typography variant="body2" color="textSecondary" align="center">
-            {"Copyright © "}
-            <Link color="inherit" href="https://womenofcolour.org.au/">
-                Women of Colour
-            </Link>{" "}
-            {new Date().getFullYear()}
-            {"."}
-        </Typography>
-    );
-}
+import Copyright from '../../components/copyright/Copyright'
 
 const useStyles = makeStyles((theme) => ({
     paper: {
