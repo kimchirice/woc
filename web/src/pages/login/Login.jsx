a000d0b59f55ba6c7eebdc6676be0756cb84c6b1
Max Du <kimchi@u12.co>
2021-05-05 10:19:51 +1000

    [Rem] remove CssBaseline from Login page
    CssBaseline is setup in index.js

--- web/src/pages/login/Login.jsx
+++ web/src/pages/login/Login.jsx
@@ -2,7 +2,6 @@ import React, {useState} from 'react';
 import { Link as RouterLink } from 'react-router-dom'
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
@@ -71,7 +70,6 @@ export default function SignIn() {
 
   return (
     <Container component="main" maxWidth="xs">
-      <CssBaseline />
       <div className={classes.paper}>
         <Avatar className={classes.avatar}>
           <LockOutlinedIcon />
