decb25f03b18f15e318f46d3665dc6db642c7b4a
Max Du <kimchi@u12.co>
2021-05-05 10:14:29 +1000

    [Mod] use RouterLink from react-router-dom to fix the page reload
    when user click the link of Sign In on SignUp page, the browser refresh

--- web/src/pages/signUp/SignUp.jsx
+++ web/src/pages/signUp/SignUp.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link as RouterLink } from 'react-router-dom'
 import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
@@ -139,7 +140,7 @@ export default function SignUp() {
 
                     <Grid container justify="flex-end">
                         <Grid item>
-                            <Link href="/login" variant="body2">
+                            <Link component={ RouterLink } to="/login" variant="body2">
                                 Already have an account? Sign in
                             </Link>
                         </Grid>
